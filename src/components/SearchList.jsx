124b3aa24783cdcf325e1b67ddd946ade5d7e7c1
Nash <nashbach69@gmail.com>
2022-12-27 04:27:47 +0530

    Added search recommendation

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import axios from "axios";
 
-const SearchList = ({ data, clicker }) => {
-  const getWeather = () => {
+const SearchList = ({ data, clicker, setCity }) => {
+  var location = "";
+  const getWeather = (event) => {
+    location = event.target.innerText;
+    setCity("");
+    console.log(data.district, data.city, data.country);
     clicker(true);
     const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric`;
+    const dummy = "";
     axios
       .get(URL)
       .then((resp) => {
--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -5,40 +5,45 @@ import SearchList from "./SearchList";
 const Temp = () => {
   const [city, setCity] = useState("London");
   const [searchResults, setSearchResults] = useState([]);
-  const [formSubmit, setFormSubmit] = useState(false);
   const [clicked, setClicked] = useState(true);
+
   useEffect(() => {
     var config = {
       method: "get",
       url: `https://api.geoapify.com/v1/geocode/autocomplete?text=${city}&apiKey=48b2131fa3f942de8abc6e39cbb1528f`,
       headers: {},
     };
-    axios(config)
-      .then((resp) => {
-        setSearchResults(
-          resp.data.features.map((query, index) => ({
-            key: index,
-            address:
-              query.properties.address_line1 +
-              " " +
-              query.properties.address_line2,
-            lat: query.properties.lat,
-            lon: query.properties.lon,
-          }))
-        );
-        setClicked(false);
-      })
-      .catch((err) => {
-        throw new Error(err);
-      });
-  }, [formSubmit]);
+    if (city.length != 0) {
+      setTimeout(() => {
+        axios(config)
+          .then((resp) => {
+            setSearchResults(
+              resp.data.features
+                .filter((item, index) => index < 4)
+                .map((query, index) => ({
+                  key: index,
+                  address: query.properties.formatted,
+                  district: query.properties.address_line1,
+                  city: query.properties.city,
+                  country: query.properties.country,
+                  lat: query.properties.lat,
+                  lon: query.properties.lon,
+                }))
+            );
+            setClicked(false);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, 100);
+    }
+  }, [city]);
 
   return (
     <div>
       <form
         onSubmit={(event) => {
           event.preventDefault();
-          setFormSubmit((prev) => !prev);
         }}
         className="p-4"
       >
@@ -54,7 +59,11 @@ const Temp = () => {
         <ul className="px-4 space-y-3">
           {searchResults.map((res) => (
             <li key={res.key} className="cursor-pointer">
-              <SearchList data={res} clicker={setClicked}></SearchList>
+              <SearchList
+                data={res}
+                clicker={setClicked}
+                setCity={setCity}
+              ></SearchList>
             </li>
           ))}
         </ul>
